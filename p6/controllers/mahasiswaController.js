0f7325be8daa79ac55a687645107c09b871b4e81
ijeny <imtinan.jmb6628@gmail.com>
2026-04-22 09:31:44 +0700

    update

--- p6/controllers/mahasiswaController.js
+++ p6/controllers/mahasiswaController.js
@@ -1,4 +1,4 @@
-const Mahasiswa = require("../models/Mahasiswa");
+const Mahasiswa = require("../models/mahasiswa");
 
 module.exports = {
   viewMahasiswa: async (req, res) => {
@@ -9,6 +9,72 @@ module.exports = {
         mahasiswa,
         title: "CRUD",
       });
+    } catch (error) {
+      console.log(error);
+      res.send(error.message);
+    }
+  },
+
+  viewAddMahasiswa: (req, res) => {
+    res.render("add", {
+      title: "Tambah Mahasiswa",
+    });
+  },
+
+  addMahasiswa: async (req, res) => {
+    try {
+      await Mahasiswa.create({
+        nama: req.body.nama,
+        nim: req.body.nim,
+        jurusan: req.body.jurusan,
+        alamat: req.body.alamat,
+      });
+
+      res.redirect("/mahasiswa");
+    } catch (error) {
+      console.log(error);
+      res.send(error.message);
+    }
+  },
+
+  viewEditMahasiswa: async (req, res) => {
+    try {
+      const mahasiswa = await Mahasiswa.findById(req.params.id);
+
+      if (!mahasiswa) {
+        return res.status(404).send("Data mahasiswa tidak ditemukan");
+      }
+
+      res.render("edit", {
+        mahasiswa,
+        title: "Edit Mahasiswa",
+      });
+    } catch (error) {
+      console.log(error);
+      res.send(error.message);
+    }
+  },
+
+  editMahasiswa: async (req, res) => {
+    try {
+      await Mahasiswa.findByIdAndUpdate(req.params.id, {
+        nama: req.body.nama,
+        nim: req.body.nim,
+        jurusan: req.body.jurusan,
+        alamat: req.body.alamat,
+      });
+
+      res.redirect("/mahasiswa");
+    } catch (error) {
+      console.log(error);
+      res.send(error.message);
+    }
+  },
+
+  deleteMahasiswa: async (req, res) => {
+    try {
+      await Mahasiswa.findByIdAndDelete(req.params.id);
+      res.redirect("/mahasiswa");
     } catch (error) {
       console.log(error);
       res.send(error.message);
--- p6/routes/mahasiswa.js
+++ p6/routes/mahasiswa.js
@@ -2,5 +2,10 @@ const router = require("express").Router();
 const mahasiswaController = require("../controllers/mahasiswaController");
 
 router.get("/", mahasiswaController.viewMahasiswa);
+router.get("/add", mahasiswaController.viewAddMahasiswa);
+router.post("/add", mahasiswaController.addMahasiswa);
+router.get("/edit/:id", mahasiswaController.viewEditMahasiswa);
+router.post("/edit/:id", mahasiswaController.editMahasiswa);
+router.get("/delete/:id", mahasiswaController.deleteMahasiswa);
 
 module.exports = router;
